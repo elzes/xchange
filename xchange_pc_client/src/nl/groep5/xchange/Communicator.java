271724546712c0e4d4e40d3372d60a884f714331
Marthyn Elzes <marthynelzes@gmail.com>
2012-12-19 13:06:25 +0100

    ad

---
+++ xchange_pc_client/src/nl/groep5/xchange/Communicator.java
@@ -0,0 +1,15 @@
+package nl.groep5.xchange;
+
+import java.util.ArrayList;
+
+import nl.groep5.xchange.models.Peer;
+
+public class Communicator
+{
+    ArrayList<Peer> peers = new ArrayList<Peer>();
+
+    public static void updatePeers()
+    {
+
+    }
+}
--- xchange_pc_client/src/nl/groep5/xchange/controllers/PeersController.java
+++ xchange_pc_client/src/nl/groep5/xchange/controllers/PeersController.java
@@ -1,8 +1,25 @@
 package nl.groep5.xchange.controllers;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.scene.control.Control;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import nl.groep5.xchange.Communicator;
 
 public class PeersController extends Control
 {
 
+    @FXML
+    protected void handleTabChange(Event event)
+    {
+        Tab tab = (Tab) event.getSource();
+        if (tab.isSelected())
+        {
+            Communicator.updatePeers();
+        }
+
+        @SuppressWarnings("unchecked")
+        ListView<Object> peers = (ListView<Object>) lookup("#peersListView");
+    }
 }
